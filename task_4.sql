ca354507df9a6b89e878dbd57ac63950db58a60c
Nathan Orang'o <nathanorango0@gmail.com>
2025-07-19 17:37:38 +0300

    Fixed errors

--- task_4.sql
+++ task_4.sql
@@ -1,9 +1,10 @@
 -- Task 4: Full description
 -- File: task_4.sql
--- This script prints the full description of the books table from alx_book_store database without using DESCRIBE or EXPLAIN statements
+-- This script prints the full description of the books table from alx_book_store database
+-- Without using DESCRIBE or EXPLAIN statements
 
 -- Use the alx_book_store database
 USE alx_book_store;
 
 -- Show columns from the books table (alternative to DESCRIBE)
-SHOW COLUMNS FROM Books;
+SHOW COLUMNS FROM books;
