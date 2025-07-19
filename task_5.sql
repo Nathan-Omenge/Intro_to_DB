6c0f45f11348909c1936ce4a6db024a2085d0684
Nathan Orang'o <nathanorango0@gmail.com>
2025-07-19 17:50:33 +0300

    Fixed errors in logic

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,7 @@
+-- Task 5: Populating Your Database with Your Very First Data
 -- Use the alx_book_store database
 USE alx_book_store;
 
 -- Insert the customer data
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
