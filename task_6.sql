f9b48cde57e75f8dda52d59eab710b87a87d8e84
Nathan Orang'o <nathanorango0@gmail.com>
2025-07-19 17:54:36 +0300

    Fixed Errors

--- task_6.sql
+++ task_6.sql
@@ -1,10 +1,13 @@
 -- Task 6: Populating Your Database with Your More Data
+-- File: task_6.sql
+-- This script inserts multiple rows in the customer table
+
 -- Use the alx_book_store database
 USE alx_book_store;
 
 -- Insert multiple customer records
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
